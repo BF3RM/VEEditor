56811ae4c1ce0c942b71f1e1263a6a30fae87d4a
Daniel Handayan Mayer <76592751+IllustrisJack@users.noreply.github.com>
2022-01-25 01:44:57 +0100

    Delete .vu/intermediate directory

--- .vu/intermediate/Shared/__init__.lua
+++
@@ -1,9 +0,0 @@
---[[
-    Venice Unleashed - Intermediate Lua binding
-    Type: __init__.lua
-    Generated on: Tue Jan 25 2022 00:26:26 GMT+0100 (Mitteleuropäische Normalzeit)
---]]
-
----@meta
-
-
